c1746fafb04df13205948efa81dae1852549b311
mr-subroutine <darrylshack@outlook.com>
2020-05-05 17:51:02 -0700

    designed intro form

---
+++ PatientRegApp1/FormIntro.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PatientRegApp1
+{
+    partial class FormIntro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.btn_cancel1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New Patient Registration v1.0";
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(28, 173);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(125, 31);
+            this.btn_create.TabIndex = 1;
+            this.btn_create.Text = "Create New Account";
+            this.btn_create.UseVisualStyleBackColor = true;
+            // 
+            // btn_cancel1
+            // 
+            this.btn_cancel1.Location = new System.Drawing.Point(159, 173);
+            this.btn_cancel1.Name = "btn_cancel1";
+            this.btn_cancel1.Size = new System.Drawing.Size(125, 31);
+            this.btn_cancel1.TabIndex = 2;
+            this.btn_cancel1.Text = "Cancel";
+            this.btn_cancel1.UseVisualStyleBackColor = true;
+            // 
+            // FormIntro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 260);
+            this.Controls.Add(this.btn_cancel1);
+            this.Controls.Add(this.btn_create);
+            this.Controls.Add(this.label1);
+            this.Name = "FormIntro";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_create;
+        private System.Windows.Forms.Button btn_cancel1;
+    }
+}
+
